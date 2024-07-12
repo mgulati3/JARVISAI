e490e1a6262e36c3a13f2c28e0bf08262902ab07
mgulati3 <162236487+mgulati3@users.noreply.github.com>
2024-07-12 01:44:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+import datetime
+
+def say(text):
+    os.system(f"say {text}")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            print("Recognizing......")
+            query = r.recognize_google(audio, language='en-US')
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return " Some error Occurred. Sorry from Jarvis"
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+    say("Hello I am jarvis A.I")
+    while True:
+        print("Listening.......")
+        query = takeCommand()
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"], ["instagram", "https://www.instagram.com"], ["github", "https://www.github.com"], ["spotify", "https://www.spotify.com"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir......")
+                webbrowser.open(site[1])
+        # say(query)
+        if "the time" in query:
+            strfTime = datetime.datetime.now().strftime("%H:%M:%S")
+            say(f"Sir the time is {strfTime}")
+
