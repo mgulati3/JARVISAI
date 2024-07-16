4a194d01248027d57e8e1021b95e3c3214b825bf
mgulati3 <162236487+mgulati3@users.noreply.github.com>
2024-07-15 23:32:32 -0700

    Update openaitest.py

--- openaitest.py
+++ openaitest.py
@@ -4,9 +4,15 @@ from config import apikey
 
 openai.api_key = apikey
 
+text = f"Openai response for prompt: Write an email to my boss for resignation \n *********************** \n\n"
+
+
 response = openai.chat.completions.create(
     messages=[{"role": "user", "content": "Write an email to my boss for resignation"}],
     model="gpt-4o",
 )
 
-print(response)
+text += response.choices[0].message.content
+
+print(text)
+
